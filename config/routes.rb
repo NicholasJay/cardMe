2cc87204fd64e3962f66fa94e8146398e94a8ce5
h4w5 <philip@h4w5.com>
2014-03-28 16:09:26 -0400

    Pjs Suggestions

--- config/routes.rb
+++ config/routes.rb
@@ -44,4 +44,46 @@ Cardme::Application.routes.draw do
   delete "/logout", to: "session#destroy"
 
   get "/card_dashboard/:id", to: "cards#card_dashboard"
+
+  # # PJ: Below is a suggeseted direction for your routes, to make them fit a more 
+  # # RESTful convention. For more, see: http://guides.rubyonrails.org/routing.html
+  
+  # # your groups interface is very simple, and you got it mostly right...
+  # root "welcome#index"
+
+  # # singular resource, maybe not with new... for the invites you can post
+  # # the necessary data to let it know the request is an invite-session request
+  # resource  :session, only: [:new, :create, :destroy]
+
+  # resources :users, shallow: true do # possible as long as the nested routes below
+  #                                    # are only ever accessed in the context of a
+  #                                    # user, such as the current user
+
+  #   resources :cards     # full CRUD on the user's own personal cards
+
+  #   # nearly full CRUD on connections... so here is the deal:
+  #   # A CONNECTION REPRESENTS A CARD HELD BY ANOTHER USER, thus
+  #   # it is always in the context of a user, but may also be
+  #   # accessed by its relationship to a user's groups (see below)
+  #   # and needn't enclose a card route, instead it IS a card...
+  #   resources :connections, except: [:new]          # a card is sent to you...
+  #   get "connections/invite", to: "connections#new" # providing us full CRUD on 'cards'
+  #                                                   # that aren't the user's, ie
+  #                                                   # 'connections'
+    
+  #   resources :groups do # full CRUD on the user's own groups
+
+  #     # these routes refer to a connection's relationship to a group...
+  #     # and all you can do is list the connections in a group
+  #     resources :connections, only: [:create, :index, :destroy], to: "groups_connections"
+  #   end
+  # end
+
+  # resources :invites, only: [:create, :show]
+
+  # # outside the contest of a user or a connection, you can still possibly see all
+  # # cards or just one card... not sure if this is necessary
+  # # resources :cards, only: [:index, :show]
+
+  # # no need for a card dashboard: instead it's a connection#show
 end
